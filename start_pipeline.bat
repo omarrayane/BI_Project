f06b81b12e8637b8e1042597520eeb8d5f1837b7
omarrayane <155535682+omarrayane@users.noreply.github.com>
2025-12-19 23:33:02 +0100

    Update start_pipeline

--- start_pipeline.bat
+++ start_pipeline.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Script pour lancer le pipeline avec le bon interpréteur Python
 echo Lancement du pipeline BI...
-"C:\Users\Aminos\AppData\Local\Programs\Python\Python313\python.exe" scripts/main.py
+"C:\Users\HP\AppData\Local\Microsoft\WindowsApps\python.exe" scripts/main.py
 if %ERRORLEVEL% NEQ 0 (
     echo.
     echo Une erreur est survenue wess.
