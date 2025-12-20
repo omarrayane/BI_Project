d54176ed34b6790ed525fa48415df34ec798631e
omarrayane <155535682+omarrayane@users.noreply.github.com>
2025-12-20 13:33:24 +0100

    Update

--- check_drivers.py
+++
@@ -1,4 +0,0 @@
-import pyodbc
-print("Installed ODBC Drivers:")
-for d in pyodbc.drivers():
-    print(d)
--- test_db.py
+++
@@ -1,8 +0,0 @@
-import pyodbc
-server = r"LAPTOP-6TJCC457\OMARRAYANE"
-try:
-    conn = pyodbc.connect(f"DRIVER={{SQL Server}};SERVER={server};DATABASE=master;Trusted_Connection=yes;")
-    print("Connected to master successfully")
-    conn.close()
-except Exception as e:
-    print("Failed to connect to master:", e)
